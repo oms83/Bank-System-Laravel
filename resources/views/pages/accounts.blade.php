b4c47d1c75bd17195e5f905f617d566d59acced2
Omer Memes <omar.memes20@gmail.com>
2023-05-24 20:24:15 +0300

    Update Bank Account

--- resources/views/pages/accounts.blade.php
+++ resources/views/pages/accounts.blade.php
@@ -189,4 +189,74 @@
         </div>
     </div>
 
+
+    
+<!-- Update New Bank Account -->
+
+<div class="modal fade" id="editBankAccountModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+
+        <form method="POST" action="" id="updateBankAccount">
+            @csrf
+
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Update Bank Account</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body p-5">
+
+                    <div class="form-group">
+                        <label for="transactionCodeInput">Account Name</label>
+                        <input type="text" id="ed_account_name" class="form-control" id="transactionCodeInput" aria-describedby="transactionCodeInputHelp" name="name" required value="Omer MEMES" />
+                    </div>
+
+                    <div class="form-group">
+                        <label for="transactionNarrationInput">Account Number</label>
+                        <input type="text" id="ed_account_number" class="form-control" id="transactionNarrationInput" aria-describedby="transactionNarrationInputHelp" name="number" required value="7894561566156" />
+                    </div>
+
+                    <div class="form-group">
+                        <label>Available Balance</label>
+                        <input type="number" id="ed_available_balance" class="form-control" name="available_balance" required value="465" />
+                    </div>
+
+                    <div class="form-group">
+                        <label>Ledger Balance</label>
+                        <input type="number" id="ed_ledger_balance" class="form-control" name="ledger_balance" required value="32131" />
+                    </div>
+
+                    <div class="form-group">
+                        <label for="usersFormControlSelect">Bank Name</label>
+                        <select class="form-control" id="banksControlSelect1" name="bank_id">
+                                <option value="1"> x </option>
+                                <option value="2"> y </option>
+                                <option value="3"> z </option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="usersFormControlSelect">Bank Branch Location</label>
+                        <select class="form-control" id="bankLocationsControlSelect1" name="bank_location_id">
+                                <option value="1"> x </option>
+                                <option value="2"> y </option>
+                                <option value="3"> z </option>
+                        </select>
+                    </div>
+
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary"> <i class="fa fa-save"></i> Update </button>
+                </div>
+            </div>
+
+        </form>
+
+    </div>
+</div>
+
 @endsection
