f9fb680c52a4cd0d98056b67ca6acce1137c8bde
Shafi Ullah Mansoori <105662822+shafimansoori@users.noreply.github.com>
2023-05-29 15:29:59 +0300

    Create CreateCurrenciesTable.php

---
+++ database/migrations/CreateCurrenciesTable.php
@@ -0,0 +1,31 @@
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCurrenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->unique();
+            $table->string('symbol');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+}
