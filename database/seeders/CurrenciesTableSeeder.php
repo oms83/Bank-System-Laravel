993ea4e73c0fe228fc8abbd58ea548ff72647a61
Shafi Ullah Mansoori <105662822+shafimansoori@users.noreply.github.com>
2023-05-29 15:31:48 +0300

    Create CurrenciesTableSeeder.php

---
+++ database/seeders/CurrenciesTableSeeder.php
@@ -0,0 +1,44 @@
+use Illuminate\Database\Seeder;
+public function run()
+    {
+
+
+        \DB::table('currencies')->delete();
+
+        \DB::table('currencies')->insert(array (
+            0 =>
+            array (
+                'id' => 1,
+                'name' => 'US Dollars',
+                'symbol' => '$',
+                'created_at' => '2023-03-23 08:19:21',
+                'updated_at' => '2023-03-23 08:19:21',
+            ),
+            1 =>
+            array (
+                'id' => 2,
+                'name' => 'Turk Lira',
+                'symbol' => '₺',
+                'created_at' => '2023-03-23 08:19:21',
+                'updated_at' => '2023-03-23 08:19:21',
+            ),
+            2 =>
+            array (
+                'id' => 3,
+                'name' => 'Pound',
+                'symbol' => '£',
+                'created_at' => '2023-03-23 08:19:21',
+                'updated_at' => '2023-03-23 08:19:21',
+            ),
+            3 =>
+                array (
+                    'id' => 4,
+                    'name' => 'Euro',
+                    'symbol' => '€',
+                    'created_at' => '2023-03-23 08:19:21',
+                    'updated_at' => '2023-03-23 08:19:21',
+                ),
+        ));
+
+
+    }
