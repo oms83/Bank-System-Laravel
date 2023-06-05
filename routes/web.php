14adff52a9a700477ca3087384494669e5b17038
Omer Memes <omar.memes20@gmail.com>
2023-06-05 18:30:05 +0300

    Update Web.php page / routes page

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\CardTypeController;
 use App\Http\Controllers\BankController;
 use App\Http\Controllers\BankLocationController;
 use App\Http\Controllers\CardTransactionController;
+use App\Http\Controllers\SandBox\SmsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,6 +28,7 @@ use App\Http\Controllers\CardTransactionController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
 
 
 // it includes  a register|login|logout routes
@@ -50,33 +52,89 @@ Route::get('/reset/account', static function () {
 })->name("reset_account");
 
 
-Route::get('/dashboard',[DashBoardController::class,'index'])->name('dashboard');
-Route::get('/profile',[ProfileController::class,'index'])->name('profile');
+
+//SandBox Section
+Route::get('/sandbox/send/sms', static function () {
+    return view('sandbox.sms');
+})->name("send_sms_sandbox");
+Route::post('/sandbox/process/sms',[SmsController::class,'processMessage'])->name('process_sms_sandbox');
 
 // //  str, array
 Route::get('/', [PagesController::class, 'index'])->name('index');
 
-Route::get('/accounts',[BankAccountController::class,'index'])->name('accounts');
-Route::get('/cards',[CardController::class,'index'])->name('cards');
 
-Route::get('/transections',[BankAccountController::class,'all_transactions'])->name('all_transactions');
+Route::group(array('middleware' => 'auth'), static function(){
+
+    Route::get('/dashboard',[DashBoardController::class,'index']);
+    Route::get('/dashboard',[DashBoardController::class,'index'])->name('dashboard');
+
+    //Bank Account Management
+    Route::get('/accounts',[BankAccountController::class,'index'])->name('accounts');
+    Route::post('/account',[BankAccountController::class,'store'])->name('account');
+    Route::post('/account/update/{id}',[BankAccountController::class,'update'])->name('update_account');
+    Route::get('/account/delete/{id}',[BankAccountController::class,'destory'])->name('delete_account');
+    Route::get('/account/restore/{id}',[BankAccountController::class,'restore'])->name('restore_account');
+
+    Route::get('/account/transactions/{id}',[BankAccountController::class,'transactions'])->name('account_history');
+    Route::get('/transactions',[BankAccountController::class,'all_transactions'])->name('all_transactions');
+    Route::post('/bank/transaction',[BankAccountController::class,'storeTransaction'])->name('add_bank_transaction');
 
 
-Route::get('/settings',[SettingsController::class,'index'])->name('settings');
+    //Cards Management
+    Route::get('/cards',[CardController::class,'index'])->name('cards');
+    Route::post('/card',[CardController::class,'store'])->name('card');
+    Route::get('/card/transactions/{id}',[CardTransactionController::class,'show'])->name('card_transactions');
+    Route::post('/card/update/{id}',[CardController::class,'update'])->name('update_card');
+    Route::get('/card/delete/{id}',[CardController::class,'destory'])->name('delete_card');
+    Route::get('/card/restore/{id}',[CardController::class,'restore'])->name('restore_card');
+    Route::post('/card/ransaction',[CardTransactionController::class,'storeTransaction'])->name('add_card_transaction');
 
-Route::get('/users',[UserController::class,'index'])->name('users');
-Route::post('/user/save',[UserController::class,'store'])->name('save_user');
-Route::post('/user/update/{id}',[UserController::class,'update'])->name('edit_user');
+    //Profile Management
+    Route::get('/profile',[ProfileController::class,'index'])->name('profile');
 
-Route::get('/currencies',[CurrencyController::class,'index'])->name('currencies');
-Route::post('/currency/save',[CurrencyController::class,'store'])->name('save_currency');
+    //Setting Management
+    Route::get('/settings',[SettingsController::class,'index'])->name('settings');
+    Route::post('/settings',[SettingsController::class,'update'])->name('update_settings');
 
 
-Route::get('/inbox',[MessageController::class,'index'])->name('inbox');
+    //Inbox Messages
+    Route::get('/inbox',[MessageController::class,'index'])->name('inbox');
+    Route::get('/inbox/{id}',[MessageController::class,'show'])->name('read_message');
+    Route::post('/send/message',[MessageController::class,'store'])->name('send_message');
 
-Route::get('/card_types',[CardTypeController::class,'index'])->name('card_types');
-Route::get('/banks',[BankController::class,'index'])->name('banks');
 
-Route::get('/bank/locations',[BankLocationController::class,'index'])->name('bank_locations');
+    //User Account Management
+    Route::get('/users',[UserController::class,'index'])->name('users');
+    Route::get('/user/delete/{id}',[UserController::class,'destory'])->name('delete_user');
+    Route::get('/user/restore/{id}',[UserController::class,'restore'])->name('restore_user');
+    Route::post('/user/save',[UserController::class,'store'])->name('save_user');
+    Route::post('/user/update/{id}',[UserController::class,'update'])->name('edit_user');
+    Route::post('/user/password/update/{id}',[UserController::class,'change_password'])->name('change_password');
 
-Route::get('/card/transactions/{id}',[CardTransactionController::class,'show'])->name('card_transactions');
+
+    //Currencies
+    Route::get('/currencies',[CurrencyController::class,'index'])->name('currencies');
+    Route::post('/currency/save',[CurrencyController::class,'store'])->name('save_currency');
+    Route::post('/currency/update/{id}',[CurrencyController::class,'update'])->name('edit_currency');
+
+    //Card Types
+    Route::get('/card/types',[CardTypeController::class,'index'])->name('card_types');
+    Route::post('/card/types/save',[CardTypeController::class,'store'])->name('save_card_type');
+    Route::post('/card/types/update/{id}',[CardTypeController::class,'update'])->name('edit_card_type');
+
+    //Banks
+    Route::get('/banks',[BankController::class,'index'])->name('banks');
+    Route::post('/bank/save',[BankController::class,'store'])->name('save_bank');
+    Route::post('/bank/update/{id}',[BankController::class,'update'])->name('edit_bank');
+    Route::get('/bank/delete/{id}',[BankController::class,'destory'])->name('delete_bank');
+    Route::get('/bank/restore/{id}',[BankController::class,'restore'])->name('restore_bank');
+
+
+    //Bank Locations
+    Route::get('/bank/{id}/locations',[BankLocationController::class,'index'])->name('bank_locations');
+    Route::post('/bank/location/save',[BankLocationController::class,'store'])->name('save_bank_location');
+    Route::post('/bank/location/update/{id}',[BankLocationController::class,'update'])->name('edit_bank_location');
+    Route::get('/bank/location/delete/{id}',[BankLocationController::class,'destory'])->name('delete_bank_location');
+    Route::get('/bank/location/restore/{id}',[BankLocationController::class,'restore'])->name('restore_bank_location');
+
+});
